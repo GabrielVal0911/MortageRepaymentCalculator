077572a49829c2ed63a7fd0fd7a5b30575562d7e
GabrielVal0911 <gabrielvalentin091101@yahoo.com>
2025-03-19 19:34:20 +0200

    Added a formula to calculate mortgage

--- script.js
+++ script.js
@@ -16,16 +16,35 @@ function clearAll() {
   inputInterestOnly.checked = false;
 }
 
+function formatNumber(num) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(num);
+}
+
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = Number(inputMortgageAmount.value);
+  const amount = Number(inputMortgageAmount.value.replace(/[.,]/g, ""));
   const term = Number(inputMortgageTerm.value) * 12;
   const interestRate = Number(inputInterestRate.value) / 100 / 12;
 
-  //   console.log(
-  //     (amount * interestRate * (1 + interestRate) ** term) /
-  //       (1 + interestRate) ** term -
-  //       1
-  //   );
+  const paymentMonthly =
+    (amount * (interestRate * (1 + interestRate) ** term)) /
+    ((1 + interestRate) ** term - 1);
+
+  const totalRepayment = paymentMonthly * term;
+
+  const monthlyPaymentInterestOnly = amount * interestRate;
+  const totalPaymentInterestOnly = monthlyPaymentInterestOnly * term + amount;
+
+  if (inputRepayment.checked) {
+    console.log(formatNumber(paymentMonthly));
+    console.log(`monthly payment: ${paymentMonthly}`);
+    console.log(`total repayment: ${totalRepayment}`);
+  } else if (inputInterestOnly.checked) {
+    console.log(`monthly interest only: ${monthlyPaymentInterestOnly}`);
+    console.log(`total interest: ${totalPaymentInterestOnly}`);
+  }
 });
